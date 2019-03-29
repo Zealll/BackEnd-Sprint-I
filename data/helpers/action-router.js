f2bd773351ae1df2f9e5f362de1083c46dc9b4c8
Zealll <eriznis@gmail.com>
2019-03-29 12:03:05 -0400

    GET by ID created for Actions

--- data/helpers/action-router.js
+++ data/helpers/action-router.js
@@ -21,7 +21,27 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id
 
+    actions
+    .get(id)
+    .then(action => {
+        if(action.length === 0) {
+            res
+            .status(404)
+            .json({message: `Action with the specified ID of ${id} does not exist.`})
+        } else {
+            res.json(action)
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res
+        .status(500)
+        .json({message: "Something went wrong with the server while retrieving your Action."})
+    })
+})
 
 
 
