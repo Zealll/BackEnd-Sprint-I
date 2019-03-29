3d1766477eee4aee4a264e94cf0ca7b543cccf4b
Zealll <eriznis@gmail.com>
2019-03-29 11:27:51 -0400

    Started on Projects, GET endpoint created

--- data/helpers/project-router.js
+++ data/helpers/project-router.js
@@ -5,7 +5,22 @@ const router = express.Router()
 const projects = require('./projectModel.js')
 
 
+router.get('/', (req, res) => {
 
+    projects
+    .get()
+    .then(projects => {
+        res
+        .status(200)
+        .json(projects)
+    })
+    .catch(error => {
+        console.log(error)
+        res
+        .status(500)
+        .then({message: "Sorry something went wrong while getting Projects."})
+    })
+})
 
 
 
