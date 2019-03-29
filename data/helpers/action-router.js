9c9b73943481494ceeb9c20009aad6dc7b17536a
Zealll <eriznis@gmail.com>
2019-03-29 11:58:55 -0400

    GET for Actions created

--- data/helpers/action-router.js
+++ data/helpers/action-router.js
@@ -6,5 +6,23 @@ const actions = require('./actionModel.js')
 
 
 
+router.get('/', (req, res) => {
+
+    actions
+    .get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(error => {
+        console.log(error)
+        res
+        .status(500)
+        .json({message: "Sorry something went wrong while getting actions."})
+    })
+})
+
+
+
+
 
 module.exports = router
